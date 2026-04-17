f3c4b062e701c64c6ff7479a87bfe2d0002b77fc
remi tardif <remitardif11@gmail.com>
2026-04-17 12:24:27 +0200

    add k6 eaasy

--- k6/index.js
+++ k6/index.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { Rate } from 'k6/metrics';
 
-const failureRate = new Rate('failed requests');
+const failureRate = new Rate('failed_requests');
 
 export function test_api_endpoints_config() {
     let res = http.get(`http://localhost:8080/books`);
